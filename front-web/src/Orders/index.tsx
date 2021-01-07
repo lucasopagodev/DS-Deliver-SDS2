414522bac741566130d8b18fabbb7127ac72ef5c
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-01-07 00:39:34 -0300

    API Connection Orders

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
+import { fetchProducts } from './api';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import Footer from '../Footer';
 
 function Orders() {
+  const [products, setProducts] = useState<Product []>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+  
   return (
-    <div className="orders-container">
-      <StepsHeader/>
-    </div>
+    <>
+      <div className="orders-container">
+        <StepsHeader/>
+        <ProductsList products={products}/>
+      </div>
+    </>
   )
 }
 
