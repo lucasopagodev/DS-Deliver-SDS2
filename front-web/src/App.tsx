07a6b5b3794d229b2ffd74aa1d274667ddd68ec6
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-01-08 17:18:43 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
 import Routes from "./Routes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
