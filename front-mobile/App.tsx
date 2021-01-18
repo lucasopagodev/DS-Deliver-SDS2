faf17a2de629e7dbce27d8dc83fd023af741ae85
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-01-18 20:31:52 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -18,8 +18,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
